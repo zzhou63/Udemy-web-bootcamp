e01ecf8626aac4e06ab42ee65f39b7e3a0b62060
zzhou63 <117297281+zzhou63@users.noreply.github.com>
2023-08-05 02:50:09 +0800

    Project 9: ToDo List v1

---
+++ todolist-v1 c/date.js
@@ -0,0 +1,22 @@
+// Completely Reuseable
+exports.getDate = function (){
+const today = new Date();
+    
+    const options = {
+        weekday: "long",
+        day: "numeric",
+        month: "long",
+    };
+    let day = today.toLocaleDateString("en-US",options);
+    return day
+};  
+
+exports.getDay = function(){
+const today = new Date();
+    
+    const options = {
+        weekday: "long",
+    };
+    return today.toLocaleDateString("en-US",options);
+}    
+
